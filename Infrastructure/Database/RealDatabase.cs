b439eaceb690468b7dcab788fc02176b746a1b80
Bubbelbad <victor.ivarson@hotmail.se>
2024-11-28 09:52:05 +0100

    Database added

---
+++ Infrastructure/Database/RealDatabase.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database
+{
+    public class RealDatabase : DbContext
+    {
+        public RealDatabase() { }
+        public RealDatabase(DbContextOptions<RealDatabase> options) : base(options) { }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=VULLE\\SQLEXPRESS; Database=CleanArchitecture; Trusted_Connection=true; TrustServerCertificate=true;");
+        //}
+    }
+}
+    
