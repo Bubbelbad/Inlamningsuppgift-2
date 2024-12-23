208c198a083a103f5efe4e7f77e184c8be05d936
Bubbelbad <victor.ivarson@hotmail.se>
2024-12-23 14:12:38 +0100

    Added validators to Library endpoints

---
+++ Application/Queries/LibraryBranchQueries/GetLibraryBranchById/GetLibraryBranchQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Queries.LibraryBranchQueries.GetLibraryBranchById
+{
+    public class GetLibraryBranchQueryValidator : AbstractValidator<GetLibraryBranchByIdQuery>
+    {
+        public GetLibraryBranchQueryValidator()
+        {
+            RuleFor(x => x.Id)
+                .Must(id => id != 0).WithMessage("Id is required, cannot be empty.");
+        }
+    }
+}
