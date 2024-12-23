ab2bfa15526b7cb473604b415fce083e1e80151f
Bubbelbad <victor.ivarson@hotmail.se>
2024-12-23 13:16:15 +0100

    Implemented UpdateLibraryBranchCommand

--- API/Controllers/LibraryBranchController.cs
+++ API/Controllers/LibraryBranchController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Commands.AuthorCommands.AddAuthor;
 using Application.Commands.LibraryBranchCommands.AddLibraryBranch;
+using Application.Commands.LibraryBranchCommands.UpdateLibraryBranch;
 using Application.Dtos.LibraryBranchDtos;
 using Application.Interfaces.RepositoryInterfaces;
 using Domain.Entities.Locations;
@@ -17,11 +18,23 @@ namespace API.Controllers
         private readonly IMediator _mediator = mediator;
         private readonly ILogger<LibraryBranchController> _logger = logger;
 
-        [Route("AddLibraryBranch")]
+        [Route("Add")]
         [HttpPost]
         public async Task<IActionResult> AddLibraryBranch(AddLibraryBranchDto dto)
         {
             var operationResult = await _mediator.Send(new AddLibraryBranchCommand(dto));
+            if (operationResult.IsSuccess)
+            {
+                return Ok(operationResult.Data);
+            }
+            return BadRequest();
+        }
+
+        [Route("Update")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateLibraryBranch(UpdateLibraryBranchDto dto)
+        {
+            var operationResult = await _mediator.Send(new UpdateLibraryBranchCommand(dto));
             if (operationResult.IsSuccess)
             {
                 return Ok(operationResult.Data);
---
+++ Application/Commands/LibraryBranchCommands/UpdateLibraryBranch/UpdateLibraryBranchCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿
+using Application.Dtos.LibraryBranchDtos;
+using Application.Interfaces.RepositoryInterfaces;
+using Application.Models;
+using AutoMapper;
+using Domain.Entities.Locations;
+using MediatR;
+
+namespace Application.Commands.LibraryBranchCommands.UpdateLibraryBranch
+{
+    public class UpdateLibraryBranchCommandHandler(IGenericRepository<LibraryBranch, int> repository, IMapper mapper) : IRequestHandler<UpdateLibraryBranchCommand, OperationResult<GetLibraryBranchDto>>
+    {
+        private readonly IGenericRepository<LibraryBranch, int> _repository = repository;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<OperationResult<GetLibraryBranchDto>> Handle(UpdateLibraryBranchCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                LibraryBranch libraryBranch = new()
+                {
+                    BranchId = request.dto.Id,
+                    Name = request.dto.Name,
+                    Location = request.dto.Location,
+                    ContactInfo = request.dto.ContactInfo
+                };
+
+                var updatedLibraryBranch = await _repository.UpdateAsync(libraryBranch);
+                var mappedLibraryBranch = _mapper.Map<GetLibraryBranchDto>(updatedLibraryBranch);
+                return OperationResult<GetLibraryBranchDto>.Success(mappedLibraryBranch);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Nope", ex);
+            }
+        }
+    }
+}
