d39fd8cef143c68652337b806f46ea1068c1e01f
Bubbelbad <victor.ivarson@hotmail.se>
2024-12-24 11:00:44 +0100

    Added Validators to BookCopy Commands

---
+++ Application/Commands/BookCopyCommands/DeleteBookCopy/DeleteBookCopyCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Commands.BookCopyCommands.DeleteBookCopy
+{
+    public class DeleteBookCopyCommandValidator : AbstractValidator<DeleteBookCopyCommand>
+    {
+        public DeleteBookCopyCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Id is required.");
+        }
+    }
+}
---
+++ Application/Commands/BookCopyCommands/UpdateBookCopy/UpdateBookCopyCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿
+using FluentValidation;
+
+namespace Application.Commands.BookCopyCommands.UpdateBookCopy
+{
+    public class UpdateBookCopyCommandValidator : AbstractValidator<UpdateBookCopyCommand>
+    {
+        public UpdateBookCopyCommandValidator()
+        {
+            RuleFor(x => x.Dto.CopyId)
+                .NotNull()
+                .WithMessage("Id can't be null.");
+
+            RuleFor(x => x.Dto.BookId)
+                .NotEmpty()
+                .WithMessage("BookId is required.");
+
+            RuleFor(x => x.Dto.BranchId)
+                .NotEmpty()
+                .WithMessage("BranchId is required.");
+
+            RuleFor(x => x.Dto.Status)
+                .NotEmpty()
+                .WithMessage("Status is required.");
+        }
+    }
+}
