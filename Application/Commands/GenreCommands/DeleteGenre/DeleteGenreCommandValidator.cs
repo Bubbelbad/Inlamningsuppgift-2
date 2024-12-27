bf14327b8738073ce3c051d3b4aba7879ecd96b1
Bubbelbad <victor.ivarson@hotmail.se>
2024-12-27 12:16:08 +0100

    Implemented Validators for Genre endpoints

---
+++ Application/Commands/GenreCommands/DeleteGenre/DeleteGenreCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Commands.GenreCommands.DeleteGenre
+{
+    public class DeleteGenreCommandValidator : AbstractValidator<DeleteGenreCommand>
+    {
+        public DeleteGenreCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Id is required");
+        }
+    }
+}
---
+++ Application/Commands/GenreCommands/UpdateGenre/UpdateGenreCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿
+using FluentValidation;
+
+namespace Application.Commands.GenreCommands.UpdateGenre
+{
+    public class UpdateGenreCommandValidator : AbstractValidator<UpdateGenreCommand>
+    {
+        public UpdateGenreCommandValidator()
+        {
+            RuleFor(x => x.Dto.Id)
+                .NotEmpty().WithMessage("Id is required");
+
+            RuleFor(x => x.Dto.Description)
+                .NotEmpty().MaximumLength(250).WithMessage("Description is required");
+
+            RuleFor(x => x.Dto.Name)
+                .NotEmpty().MaximumLength(50).WithMessage("Name is required");
+        }
+    }
+}
