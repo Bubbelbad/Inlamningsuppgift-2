cfa7db0b33b7337eeff2f837b5d63b7bf4f07b43
Bubbelbad <victor.ivarson@hotmail.se>
2024-12-31 11:14:20 +0100

    Implemented Validators on Review Endpoints

---
+++ Application/Commands/ReviewCommands/DeleteReview/DeleteReviewCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Commands.ReviewCommands.DeleteReview
+{
+    public class DeleteReviewCommandValidator : AbstractValidator<DeleteReviewCommand>
+    {
+        public DeleteReviewCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Id can not be empty");
+        }
+    }
+}
---
+++ Application/Queries/ReviewQueries/GetReviewById/GetReviewByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿
+using FluentValidation;
+
+namespace Application.Queries.ReviewQueries.GetReviewById
+{
+    public class GetReviewByIdQueryValidator : AbstractValidator<GetReviewByIdQuery>
+    {
+        public GetReviewByIdQueryValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Id can not be empty");
+        }
+    }
+}
