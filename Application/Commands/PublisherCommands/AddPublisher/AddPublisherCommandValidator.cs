e71e69d529b19b22589105b39ca32c32c05dc3e8
Bubbelbad <victor.ivarson@hotmail.se>
2025-01-01 14:19:20 +0100

    Implemented Validation on publisher endpoints

---
+++ Application/Commands/PublisherCommands/AddPublisher/AddPublisherCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Application.Commands.PublisherCommands.AddPublisher
+{
+    public class AddPublisherCommandValidator : AbstractValidator<AddPublisherCommand>
+    {
+        public AddPublisherCommandValidator()
+        {
+            RuleFor(x => x.Dto.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .MaximumLength(50).WithMessage("Name must not exceed 50 characters");
+
+            RuleFor(x => x.Dto.Address)
+                .NotEmpty().WithMessage("Address is required");
+
+            RuleFor(x => x.Dto.ContactInfo)
+                .NotEmpty().WithMessage("Contact info is required");
+        }
+    }
+}
