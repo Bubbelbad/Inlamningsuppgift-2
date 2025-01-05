dbf41bc3ac5a54680a986181aa5c988a8681c1ce
Bubbelbad <victor.ivarson@hotmail.se>
2025-01-05 16:40:04 +0100

    Improved AddReservation endpoint

--- Application/Commands/ReservationCommands/AddReservation/AddReservationCommandHandler.cs
+++ Application/Commands/ReservationCommands/AddReservation/AddReservationCommandHandler.cs
@@ -1,22 +1,42 @@
-﻿
-using Application.Dtos.ReservationDtos;
+﻿using Application.Dtos.ReservationDtos;
 using Application.Interfaces.RepositoryInterfaces;
 using Application.Models;
 using AutoMapper;
+using Domain.Entities.Core;
+using Domain.Entities.Locations;
 using Domain.Entities.Transactions;
 using MediatR;
 
 namespace Application.Commands.ReservationCommands.AddReservation
 {
-    internal class AddReservationCommandHandler(IGenericRepository<Reservation, int> repository, IMapper mapper) : IRequestHandler<AddReservationCommand, OperationResult<GetReservationDto>>
+    internal class AddReservationCommandHandler(IGenericRepository<Reservation, int> reservationRepository,
+                                                IGenericRepository<User, string> userRepository,
+                                                IGenericRepository<BookCopy, Guid> bookRepository, IMapper mapper) : IRequestHandler<AddReservationCommand, OperationResult<GetReservationDto>>
     {
-        private readonly IGenericRepository<Reservation, int> _repository = repository;
+        private readonly IGenericRepository<Reservation, int> _reservationRepository = reservationRepository;
+        private readonly IGenericRepository<User, string> _userRepository = userRepository;
+        private readonly IGenericRepository<BookCopy, Guid> _bookRepository = bookRepository;
         private readonly IMapper _mapper = mapper;
 
         public async Task<OperationResult<GetReservationDto>> Handle(AddReservationCommand request, CancellationToken cancellationToken)
         {
             try
             {
+                // Retrieve the book copy
+                var bookCopy = await _bookRepository.GetByIdAsync(request.Dto.CopyId);
+                if (bookCopy is null || bookCopy.Status == "Reserved")
+                {
+                    return OperationResult<GetReservationDto>.Failure("Book copy is not available");
+                }
+
+                //Retrieve the user
+                var user = await _userRepository.GetByIdAsync(request.Dto.UserId);
+                if (user is null)
+                {
+                    return OperationResult<GetReservationDto>.Failure("User not found");
+                }
+
+                // Create a new reservation record
                 Reservation reservation = new()
                 {
                     Status = "Reserved",
@@ -25,13 +45,21 @@ namespace Application.Commands.ReservationCommands.AddReservation
                     UserId = request.Dto.UserId
                 };
 
-                var addedReservation = await _repository.AddAsync(reservation);
-                if (addedReservation is null)
+                // Change the bookCopy status
+                var successfulUpdate = await _bookRepository.UpdateAsync(bookCopy);
+                if (successfulUpdate is null)
                 {
                     return OperationResult<GetReservationDto>.Failure("Operation failed");
                 }
 
-                var mappedReservation = _mapper.Map<GetReservationDto>(addedReservation);
+                // Save the reservation
+                var successfulReservation = await _reservationRepository.AddAsync(reservation);
+                if (successfulReservation is null)
+                {
+                    return OperationResult<GetReservationDto>.Failure("Operation failed");
+                }
+
+                var mappedReservation = _mapper.Map<GetReservationDto>(successfulReservation);
                 return OperationResult<GetReservationDto>.Success(mappedReservation);
             }
             catch (Exception ex)
