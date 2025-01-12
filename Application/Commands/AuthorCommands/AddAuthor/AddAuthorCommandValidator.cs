ed4105dba7478ed2a8c3f4ad5ece658b9fea569b
Bubbelbad <victor.ivarson@hotmail.se>
2025-01-12 12:37:13 +0100

    Fixed Add Author Integration tests that was failing

--- Application/Commands/AuthorCommands/AddAuthor/AddAuthorCommandValidator.cs
+++ Application/Commands/AuthorCommands/AddAuthor/AddAuthorCommandValidator.cs
@@ -7,10 +7,11 @@ namespace Application.Commands.AuthorCommands.AddAuthor
         public AddAuthorCommandValidator()
         {
             RuleFor(x => x.NewAuthor.FirstName)
-                .NotEmpty().WithMessage("Name is required.")
+                .NotNull().NotEmpty().WithMessage("Name is required.")
                 .Length(2, 50).WithMessage("Name must be between 2 and 50 characters.");
 
             RuleFor(x => x.NewAuthor.LastName)
+                .NotNull().WithMessage("Name is required.")
                 .NotEmpty().WithMessage("Lastname is required.")
                 .MinimumLength(3).WithMessage("Password must be at least 3 characters long.");
         }
